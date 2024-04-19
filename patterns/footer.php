408366ea8b5a8a13e599c4c48f80cd3c1851ea22
Matthew Haines-Young <matthew@humanmade.com>
2024-04-19 16:50:18 +0300

    initial commit

---
+++ patterns/footer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Title: footer
+ * Slug: h3/footer
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"layout":{"inherit":true}} -->
+<div class="wp-block-group">
+	<!-- wp:group {"style":{"spacing":{"padding":{"top":"80px","bottom":"30px"}}}} -->
+	<div class="wp-block-group" style="padding-top:80px;padding-bottom:30px">
+		<!-- wp:paragraph {"align":"center"} -->
+		<p class="has-text-align-center"><?php echo __('Proudly Powered by <a href="https://wordpress.org" rel="nofollow">WordPress</a>', 'h3');?></p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
