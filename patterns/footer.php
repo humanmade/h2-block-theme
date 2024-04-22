4e43d9f1559a95c51311bc11fcdbc40b00812dd0
Matthew Haines-Young <matthew@humanmade.com>
2024-04-22 16:31:44 +0100

    Update theme templates

--- patterns/footer.php
+++ patterns/footer.php
@@ -6,14 +6,10 @@
  * Inserter: no
  */
 ?>
-<!-- wp:group {"layout":{"inherit":true}} -->
-<div class="wp-block-group">
-	<!-- wp:group {"style":{"spacing":{"padding":{"top":"80px","bottom":"30px"}}}} -->
-	<div class="wp-block-group" style="padding-top:80px;padding-bottom:30px">
-		<!-- wp:paragraph {"align":"center"} -->
-		<p class="has-text-align-center"><?php echo __('Proudly Powered by <a href="https://wordpress.org" rel="nofollow">WordPress</a>', 'h3');?></p>
-		<!-- /wp:paragraph -->
-	</div>
-	<!-- /wp:group -->
-</div>
+<!-- wp:group {"layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"0","right":"0"}}},"fontSize":"small"} -->
+<div class="wp-block-group alignfull has-small-font-size" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--40);padding-right:0;padding-bottom:var(--wp--preset--spacing--40);padding-left:0"><!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><?php echo __('H2 Block Theme by Human Made', 'h3');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->
